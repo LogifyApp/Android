37993fb984da54cf34d87941ce0a9b8ef16494fe
Denys <shulgindenis2@gmail.com>
2024-07-11 00:15:06 +0200

    Finished CargoScreen(Fixed problems with TopAppBar and BottomAppBar)

--- app/src/main/java/com/example/logify/view/components/CustomSearchField.kt
+++ app/src/main/java/com/example/logify/view/components/CustomSearchField.kt
@@ -3,8 +3,10 @@ package com.example.logify.view.components
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
@@ -17,9 +19,12 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -36,10 +41,12 @@ fun CustomSearchField(
     onValueChange: (String) -> Unit,
     placeholder: String
 ) {
+    val keyboardController = LocalSoftwareKeyboardController.current
+
     TextField(
         value = value,
         onValueChange = onValueChange,
-        placeholder = { Text(placeholder) },
+        placeholder = { Text(placeholder, style = TextStyle(fontSize = 14.sp)) },
         trailingIcon = {
             Icon(
                 imageVector = Icons.Default.Search,
@@ -51,9 +58,10 @@ fun CustomSearchField(
             .fillMaxWidth()
             .clip(RoundedCornerShape(19.dp))
             .background(Color.White)
-            .height(55.dp),
+            .height(48.dp)
+            .clipToBounds(),
         textStyle = TextStyle(
-            fontSize = 16.sp,
+            fontSize = 14.sp,
             fontFamily = FontFamily(Font(R.font.opensans_medium)),
             fontWeight = FontWeight.Medium
         ),
@@ -67,6 +75,7 @@ fun CustomSearchField(
         ),
         keyboardActions = KeyboardActions(
             onSearch = {
+                keyboardController?.hide()
             }
         )
     )
--- app/src/main/java/com/example/logify/view/screens/CargoScreen.kt
+++ app/src/main/java/com/example/logify/view/screens/CargoScreen.kt
@@ -4,25 +4,34 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -33,8 +42,10 @@ import com.example.logify.ui.theme.BlueBar
 import com.example.logify.ui.theme.DarkBlue
 import com.example.logify.view.components.CargoList
 import com.example.logify.view.components.CustomSearchField
+import com.google.accompanist.insets.ProvideWindowInsets
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CargoScreen(cargoItems: List<Cargo>) {
     var search by remember { mutableStateOf("") }
@@ -44,84 +55,127 @@ fun CargoScreen(cargoItems: List<Cargo>) {
         filteredItems = if (search.isEmpty()) {
             cargoItems
         } else {
-            cargoItems.filter { it.id.toString().contains(search) || it.status.contains(search, ignoreCase = true) }
+            cargoItems.filter {
+                it.id.toString().contains(search) || it.status.contains(
+                    search,
+                    ignoreCase = true
+                )
+            }
         }
     }
 
-    Scaffold(
-        topBar = {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(BlueBar)
-                    .padding(24.dp)
-            ) {
-                CustomSearchField(
-                    value = search,
-                    onValueChange = { search = it },
-                    placeholder = "Search"
-                )
-            }
-        },
-        bottomBar = {
-            BottomAppBar(
-                containerColor = BlueBar,
-                contentPadding = PaddingValues(horizontal = 0.dp, vertical = 4.dp),
-            ) {
-                Spacer(modifier = Modifier.size(30.dp))
-                NavigationBarItem(
-                    icon = { Icon(painterResource(id = R.drawable.user), contentDescription = null, modifier = Modifier.size(40.dp)) },
-                    selected = true,
-                    onClick = {},
-                    colors = NavigationBarItemDefaults.colors(
-                        selectedIconColor = DarkBlue,
-                        unselectedIconColor = BlueBar,
-                        indicatorColor = Color.Transparent
-                    ),
-                    modifier = Modifier
-                        .background(Color.White, shape = RoundedCornerShape(50))
-                        .scale(1.3f)
-                )
-                Spacer(modifier = Modifier.size(60.dp))
-                NavigationBarItem(
-                    icon = { Icon(painterResource(id = R.drawable.cargo), contentDescription = null, modifier = Modifier.size(40.dp)) },
-                    selected = false,
-                    onClick = {},
-                    colors = NavigationBarItemDefaults.colors(
-                        selectedIconColor = DarkBlue,
-                        unselectedIconColor = BlueBar,
-                        indicatorColor = Color.Transparent
-                    ),
-                    modifier = Modifier
-                        .background(Color.White, shape = RoundedCornerShape(50))
-                        .scale(1.3f)
-                )
-                Spacer(modifier = Modifier.size(60.dp))
-                NavigationBarItem(
-                    icon = { Icon(painterResource(id = R.drawable.chat), contentDescription = null, modifier = Modifier.size(40.dp)) },
-                    selected = false,
-                    onClick = {},
-                    colors = NavigationBarItemDefaults.colors(
-                        selectedIconColor = DarkBlue,
-                        unselectedIconColor = BlueBar,
-                        indicatorColor = Color.Transparent
-                    ),
-                    modifier = Modifier
-                        .background(Color.White, shape = RoundedCornerShape(50))
-                        .scale(1.3f)
-                )
-                Spacer(modifier = Modifier.size(30.dp))
-
-            }
-        }
-    ) { innerPadding ->
+    ProvideWindowInsets {
         Box(
             modifier = Modifier
                 .fillMaxSize()
                 .background(BackgroundLightBlue)
-                .padding(innerPadding)
         ) {
-            CargoList(cargoItems = filteredItems)
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        colors = TopAppBarDefaults.topAppBarColors(
+                            containerColor = BlueBar
+                        ),
+                        title = {
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .background(BlueBar)
+                                    .padding(horizontal = 16.dp, vertical = 16.dp),
+                                contentAlignment = Alignment.BottomCenter
+                            ) {
+                                CustomSearchField(
+                                    value = search,
+                                    onValueChange = { search = it },
+                                    placeholder = "Search"
+                                )
+                            }
+                        },
+                        modifier = Modifier.height(130.dp)
+                    )
+                },
+                bottomBar = {
+                    BottomAppBar(
+                        containerColor = BlueBar,
+                        contentPadding = PaddingValues(horizontal = 10.dp, vertical = 10.dp),
+                    ) {
+                        Spacer(modifier = Modifier.weight(0.5f))
+                        NavigationBarItem(
+                            icon = {
+                                Icon(
+                                    painterResource(id = R.drawable.user),
+                                    contentDescription = null,
+                                    modifier = Modifier.size(30.dp)
+                                )
+                            },
+                            selected = true,
+                            onClick = {},
+                            colors = NavigationBarItemDefaults.colors(
+                                selectedIconColor = DarkBlue,
+                                unselectedIconColor = BlueBar,
+                                indicatorColor = Color.Transparent
+                            ),
+                            modifier = Modifier
+                                .background(Color.White, shape = RoundedCornerShape(50))
+                                .scale(1.5f)
+                        )
+                        Spacer(modifier = Modifier.weight(1f))
+                        NavigationBarItem(
+                            icon = {
+                                Icon(
+                                    painterResource(id = R.drawable.cargo),
+                                    contentDescription = null,
+                                    modifier = Modifier.size(30.dp)
+                                )
+                            },
+                            selected = false,
+                            onClick = {},
+                            colors = NavigationBarItemDefaults.colors(
+                                selectedIconColor = DarkBlue,
+                                unselectedIconColor = BlueBar,
+                                indicatorColor = Color.Transparent
+                            ),
+                            modifier = Modifier
+                                .background(Color.White, shape = RoundedCornerShape(50))
+                                .scale(1.5f)
+                        )
+                        Spacer(modifier = Modifier.weight(1f))
+                        NavigationBarItem(
+                            icon = {
+                                Icon(
+                                    painterResource(id = R.drawable.chat),
+                                    contentDescription = null,
+                                    modifier = Modifier.size(30.dp)
+                                )
+                            },
+                            selected = false,
+                            onClick = {},
+                            colors = NavigationBarItemDefaults.colors(
+                                selectedIconColor = DarkBlue,
+                                unselectedIconColor = BlueBar,
+                                indicatorColor = Color.Transparent
+                            ),
+                            modifier = Modifier
+                                .background(Color.White, shape = RoundedCornerShape(50))
+                                .scale(1.5f)
+                        )
+                        Spacer(modifier = Modifier.weight(0.5f))
+                    }
+                },
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(BackgroundLightBlue)
+                    .imePadding()
+            ) { innerPadding ->
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(BackgroundLightBlue)
+                        .padding(innerPadding)
+                ) {
+                    CargoList(cargoItems = filteredItems)
+                }
+            }
         }
     }
 }
